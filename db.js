fd9e082c76ebe214c14bf5a76e645e34f24d987d
Aaron Wood <aarondnwd@gmail.com>
2025-11-19 15:39:45 -0700

    Add db.js to connect API to Neon

---
+++ db.js
@@ -0,0 +1,13 @@
+// db.js — Shared Postgres connection
+
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+});
+
+module.exports = pool;
